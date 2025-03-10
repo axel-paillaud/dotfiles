3ced114072572ad4f9c42814eb915d2633e0f828
axel-paillaud <contact@axelweb.fr>
2025-03-10 09:12:47 +0100

    update print-screen script for i3: play sound if print screen is OK

--- i3/.config/i3/scripts/print-screen.sh
+++ i3/.config/i3/scripts/print-screen.sh
@@ -8,11 +8,18 @@ FULL_PATH="$SAVE_DIR/$FILENAME"
 # Créer le répertoire si nécessaire
 mkdir -p "$SAVE_DIR"
 
-# Prendre la capture d'écran avec `import` (ImageMagick)
-if command -v import >/dev/null 2>&1; then
-    import "$FULL_PATH" && notify-send "Capture d'écran" "Capture enregistrée : $FILENAME" || notify-send "Erreur" "Échec de la capture d'écran."
-else
-    notify-send "Erreur" "La commande 'import' n'est pas installée. Veuillez installer ImageMagick."
+# Vérifier si la commande `import` est disponible
+if ! command -v import >/dev/null 2>&1; then
+    notify-send "Erreur" "La commande 'import' n'est pas installée. \
+Veuillez installer ImageMagick."
     exit 1
+fi
+
+# Prendre la capture d'écran
+if import "$FULL_PATH"; then
+    notify-send "Capture d'écran" "Capture enregistrée : $FILENAME"
+    paplay /usr/share/sounds/freedesktop/stereo/camera-shutter.oga &
+else
+    notify-send "Erreur" "Échec de la capture d'écran."
 fi
 
