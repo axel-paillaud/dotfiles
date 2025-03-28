201c04a9f3c4c1cc6bbd47b780c27985857b1147
axel-paillaud <contact@axelweb.fr>
2025-03-28 13:14:45 +0100

    add rofimoji config plus i3wm keybinding

--- i3/.config/i3/scripts/print-screen.sh
+++ i3/.config/i3/scripts/print-screen.sh
@@ -15,8 +15,17 @@ Veuillez installer ImageMagick."
     exit 1
 fi
 
+# Vérifier si `xclip` est installé
+if ! command -v xclip >/dev/null 2>&1; then
+    notify-send "Erreur" "La commande 'xclip' n'est pas installée. \
+Veuillez l'installer pour copier l'image dans le presse-papier."
+fi
+
 # Prendre la capture d'écran
 if import "$FULL_PATH"; then
+    if command -v xclip >/dev/null 2>&1; then
+        xclip -selection clipboard -t image/png -i "$FULL_PATH"
+    fi
     notify-send "Capture d'écran" "Capture enregistrée : $FILENAME"
     paplay /usr/share/sounds/freedesktop/stereo/camera-shutter.oga &
 else
